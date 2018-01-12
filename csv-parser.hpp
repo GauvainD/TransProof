72efce5d37311cfc46d6beb8ee731543c74c8559
Gauvain Devillez <gauvain.devillez@umons.ac.be>
2018-01-12 11:08:33 +0100

    Cleaned error due to commit

--- csv-parser.hpp
+++ csv-parser.hpp
@@ -4,184 +4,214 @@
 #include <boost/algorithm/string/case_conv.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/variant.hpp>
+#include <vector>
+#include <regex>
+#include <map>
 
 #include "transformation_test.hpp"
 #include "graph.hpp"
 #include "graph6.hpp"
 
-namespace phoeg {
+namespace phoeg
+{
+typedef std::map<std::string, std::map<std::string, struct invariant_value> > node_list;
 
-    /**
-     * Removes spaces at the begining and end of the
-     * sring.
-     */
-    string strip(const string &str)
+/**
+ * Removes spaces at the begining and end of the
+ * sring.
+ */
+std::string strip(const std::string &str)
+{
+    std::string res = "";
+    long s = 0;
+    while (s < str.length() && str[s] == ' ')
     {
-        string res = "";
-        long s = 0;
-        while (s < str.length() && str[s] == ' ') {
-            s++;
-        }
-        long e = str.length() - 1;
-        while (e > s && str[e] == ' ') {
-            e--;
-        }
-        return str.substr(s, e - s + 1);
+        s++;
     }
-
-    /**
-     * Gives the name of the headers given in parameters.
-     * Removes the spaces at start and end of each title.
-     */
-    vector<string> parseHeader(const string &line)
+    long e = str.length() - 1;
+    while (e > s && str[e] == ' ')
     {
-        if (line.length() == 0) {
-            throw invalid_argument("headers are empty");
-        }
-        vector<string> res;
-        boost::tokenizer<boost::escaped_list_separator<char> > tok(line);
-        boost::tokenizer<boost::escaped_list_separator<char> >::iterator token = tok.begin();
-        for (; token != tok.end(); token++) {
-            res.push_back(strip(*token));
-        }
-        return res;
+        e--;
     }
+    return str.substr(s, e - s + 1);
+}
 
-    /**
-     * Escapes backslash in the signature of graphs
-     */
-    string cleanSig(const string &line)
+/**
+ * Gives the name of the headers given in parameters.
+ * Removes the spaces at start and end of each title.
+ */
+std::vector<std::string> parseHeader(const std::string &line)
+{
+    if (line.length() == 0)
     {
-        int i = 0;
-        string result = "";
-        while (i < line.length() && line[i] != ',') {
-            if (line[i] == '\\') {
-                result += "\\\\";
-            }
-            else {
-                result += line[i];
-            }
-            i++;
-        }
-        result += line.substr(i);
-        return result;
+        throw std::invalid_argument("headers are empty");
     }
-
-    /**
-     * Checks weither the given string is a graph in the g6 format
-     */
-    bool checkFormat(const string &graph)
+    std::vector<std::string> res;
+    boost::tokenizer<boost::escaped_list_separator<char> > tok(line);
+    boost::tokenizer<boost::escaped_list_separator<char> >::iterator token = tok.begin();
+    for (; token != tok.end(); token++)
     {
-        //OK if empty string
-        if (graph.size() > 0) {
-            //TODO what if n is bigger (see graph6)
-            int n = graph[0] - 63;
-            //First char must be >= 63 and <= 126
-            if (n >= 0 && n < 64) {
-                //We need to have enough chars for the matrix
-                int expected_size = (int) ceil((n * (n - 1) / 2) / 6.);
-                if (graph.size() - 1 == expected_size) {
-                    for (int i = 1; i < graph.size(); i++) {
-                        //Each char must be between 63 and 126
-                        if (graph[i] < 63 || graph[i] > 126) {
-                            return false;
-                        }
+        res.push_back(strip(*token));
+    }
+    return res;
+}
+
+/**
+ * Escapes backslash in the signature of graphs
+ */
+std::string cleanSig(const std::string &line)
+{
+    int i = 0;
+    std::string result = "";
+    while (i < line.length() && line[i] != ',')
+    {
+        if (line[i] == '\\')
+        {
+            result += "\\\\";
+        }
+        else
+        {
+            result += line[i];
+        }
+        i++;
+    }
+    result += line.substr(i);
+    return result;
+}
+
+/**
+ * Checks weither the given std::string is a graph in the g6 format
+ */
+bool checkFormat(const std::string &graph)
+{
+    //OK if empty std::string
+    if (graph.size() > 0)
+    {
+        //TODO what if n is bigger (see graph6)
+        int n = graph[0] - 63;
+        //First char must be >= 63 and <= 126
+        if (n >= 0 && n < 64)
+        {
+            //We need to have enough chars for the matrix
+            int expected_size = (int) ceil((n * (n - 1) / 2) / 6.);
+            if (graph.size() - 1 == expected_size)
+            {
+                for (int i = 1; i < graph.size(); i++)
+                {
+                    //Each char must be between 63 and 126
+                    if (graph[i] < 63 || graph[i] > 126)
+                    {
+                        return false;
                     }
-                    return true;
                 }
+                return true;
             }
-            return false;
         }
         return false;
     }
+    return false;
+}
 
-    /**
-     * Parse the given string into the correct type variable
-     * using boost::variant.
-     * Current variants are bool, float, int and string
-     */
-    struct invariant_value parseValue(const string &str)
+/**
+ * Parse the given std::string into the correct type variable
+ * using boost::variant.
+ * Current variants are bool, float, int and std::string
+ */
+struct invariant_value parseValue(const std::string &str)
+{
+    std::string lower = boost::algorithm::to_lower_copy(str);
+    struct invariant_value res;
+    std::regex f1("-?[0-9]+\\.[0-9]*");
+    std::regex f2("-?[0-9]*\\.[0-9]+");
+    std::regex i("-?[0-9]+");
+    bool match = false;
+    if (!lower.compare("true"))
     {
-        string lower = boost::algorithm::to_lower_copy(str);
-        struct invariant_value res;
-        regex f1("-?[0-9]+\\.[0-9]*");
-        regex f2("-?[0-9]*\\.[0-9]+");
-        regex i("-?[0-9]+");
-        bool match = false;
-        if (!lower.compare("true")) {
-            res.type = BOOL;
-            res.val = true;
-            match = true;
-        }
-        else if (!lower.compare("false")) {
-            res.type = BOOL;
-            res.val = false;
-            match = true;
-        }
-        else if (regex_match(str, f1) || regex_match(str, f2)) {
-            res.type = FLOAT;
-            res.val = boost::lexical_cast<double>(str);
-            match = true;
-        }
-        else if (regex_match(str, i)) {
-            res.type = INT;
-            res.val = boost::lexical_cast<long>(str);
-            match = true;
-        }
-        else if (!match) {
-            res.type = STRING;
-            res.val = str;
-        }
-        else {
-            res.type = STRING;
-            res.val = "";
-        }
-        return res;
+        res.type = BOOL;
+        res.val = true;
+        match = true;
     }
+    else if (!lower.compare("false"))
+    {
+        res.type = BOOL;
+        res.val = false;
+        match = true;
+    }
+    else if (std::regex_match(str, f1) || std::regex_match(str, f2))
+    {
+        res.type = FLOAT;
+        res.val = boost::lexical_cast<double>(str);
+        match = true;
+    }
+    else if (std::regex_match(str, i))
+    {
+        res.type = INT;
+        res.val = boost::lexical_cast<long>(str);
+        match = true;
+    }
+    else if (!match)
+    {
+        res.type = STRING;
+        res.val = str;
+    }
+    else
+    {
+        res.type = STRING;
+        res.val = "";
+    }
+    return res;
+}
 
-    /**
-     * Parse a line of a csv file.
-     * Each element is converted in one of the supported format.
-     * The results are stored in a map with the name of the headers as keys.
-     * The line will be stored with the first value as key (id)
-     */
-    string parseLine(const string &toParse, const vector<string> &keys, node_list &nodes)
+/**
+ * Parse a line of a csv file.
+ * Each element is converted in one of the supported format.
+ * The results are stored in a map with the name of the headers as keys.
+ * The line will be stored with the first value as key (id)
+ */
+std::string parseLine(const std::string &toParse, const std::vector<std::string> &keys, node_list &nodes)
+{
+    if (toParse.length() == 0)
     {
-        if (toParse.length() == 0) {
-            throw invalid_argument("line is empty");
-        }
-        string line = cleanSig(toParse);
-        vector<string> res;
-        boost::tokenizer<boost::escaped_list_separator<char> > tok(line);
-        boost::tokenizer<boost::escaped_list_separator<char> >::iterator token = tok.begin();
-        string sig = *token;
-        ++token;
-        if (!checkFormat(sig)) {
-            throw invalid_argument(sig + " is not a valid graph6");
-        }
-        int i = 1;
-        map<string, struct invariant_value> node;
-        unsigned long long int gtint = g6toInt(sig);
-        struct invariant_value val {
-            types::BIGINT, gtint
-        };
-        node.emplace(keys[0], val);
-        for (; token != tok.end(); token++) {
-            if (i > keys.size()) {
-                throw invalid_argument("Too many columns");
-            }
-            string p = strip(*token);
-            if (p.size() > 0 && p != "NaN" && p != "inf") {
-                node.emplace(keys[i], parseValue(p));
-            }
-            ++i;
-        }
-        if (i < keys.size()) {
-            throw invalid_argument("Not enough columns");
-        }
-        nodes.emplace(sig, node);
-        return sig;
+        throw std::invalid_argument("line is empty");
     }
+    std::string line = cleanSig(toParse);
+    std::vector<std::string> res;
+    boost::tokenizer<boost::escaped_list_separator<char> > tok(line);
+    boost::tokenizer<boost::escaped_list_separator<char> >::iterator token = tok.begin();
+    std::string sig = *token;
+    ++token;
+    if (!checkFormat(sig))
+    {
+        throw std::invalid_argument(sig + " is not a valid graph6");
+    }
+    int i = 1;
+    std::map<std::string, struct invariant_value> node;
+    unsigned long long int gtint = g6toInt(sig);
+    struct invariant_value val
+    {
+        types::BIGINT, gtint
+    };
+    node.emplace(keys[0], val);
+    for (; token != tok.end(); token++)
+    {
+        if (i > keys.size())
+        {
+            throw std::invalid_argument("Too many columns");
+        }
+        std::string p = strip(*token);
+        if (p.size() > 0 && p != "NaN" && p != "inf")
+        {
+            node.emplace(keys[i], parseValue(p));
+        }
+        ++i;
+    }
+    if (i < keys.size())
+    {
+        throw std::invalid_argument("Not enough columns");
+    }
+    nodes.emplace(sig, node);
+    return sig;
+}
 
 } // namespace phoeg
 
--- transproof.cpp
+++ transproof.cpp
@@ -4,11 +4,7 @@
 #include <fstream>
 #include <iostream>
 #include <cmath>
-#include <boost/tokenizer.hpp>
-#include <boost/algorithm/string/case_conv.hpp>
-#include <boost/lexical_cast.hpp>
 #include <boost/variant.hpp>
-#include <regex>
 #include <thread>
 #include <mutex>
 #include <memory>
@@ -21,6 +17,7 @@
 #include "neo4j-inserter.hpp"
 #include "csv-inserter.hpp"
 #include "graph_bin.hpp"
+#include "csv-parser.hpp"
 
 #define max(a,b) (a) > (b) ? (a) : (b)
 
@@ -31,14 +28,12 @@ typedef function<vector<struct transformation_result>(const Graph&)> transformat
 typedef function<struct filter_result(const Graph&)> filter;
 typedef map<string, map<string, struct invariant_value> > node_list;
 
-struct transformation_entry
-{
+struct transformation_entry {
     transformation transfo;
     string name;
 };
 
-struct transfo_data_result
-{
+struct transfo_data_result {
     unsigned long long int start;
     unsigned long long int end;
     string type;
@@ -55,203 +50,6 @@ shared_ptr<TransProofInserter> neo;
 int ntr = 0;
 
 /**
- * Checks weither the given string is a graph in the g6 format
- */
-bool checkFormat(const string &graph)
-{
-    //OK if empty string
-    if (graph.size() > 0)
-    {
-        //TODO what if n is bigger (see graph6)
-        int n = graph[0] - 63;
-        //First char must be >= 63 and <= 126
-        if (n >= 0 && n < 64)
-        {
-            //We need to have enough chars for the matrix
-            int expected_size = (int) ceil((n * (n - 1) / 2) / 6.);
-            if (graph.size() - 1 == expected_size)
-            {
-                for (int i = 1; i < graph.size(); i++)
-                {
-                    //Each char must be between 63 and 126
-                    if (graph[i] < 63 || graph[i] > 126)
-                    {
-                        return false;
-                    }
-                }
-                return true;
-            }
-        }
-        return false;
-    }
-    return false;
-}
-
-/**
- * Parse the given string into the correct type variable
- * using boost::variant.
- * Current variants are bool, float, int and string
- */
-struct invariant_value parseValue(const string &str)
-{
-    string lower = boost::algorithm::to_lower_copy(str);
-    struct invariant_value res;
-    regex f1("-?[0-9]+\\.[0-9]*");
-    regex f2("-?[0-9]*\\.[0-9]+");
-    regex i("-?[0-9]+");
-    bool match = false;
-    if (!lower.compare("true"))
-    {
-        res.type = BOOL;
-        res.val = true;
-        match = true;
-    }
-    else if (!lower.compare("false"))
-    {
-        res.type = BOOL;
-        res.val = false;
-        match = true;
-    }
-    else if (regex_match(str, f1) || regex_match(str, f2))
-    {
-        res.type = FLOAT;
-        res.val = boost::lexical_cast<double>(str);
-        match = true;
-    }
-    else if (regex_match(str, i))
-    {
-        res.type = INT;
-        res.val = boost::lexical_cast<long>(str);
-        match = true;
-    }
-    else if (!match)
-    {
-        res.type = STRING;
-        res.val = str;
-    }
-    else
-    {
-        res.type = STRING;
-        res.val = "";
-    }
-    return res;
-}
-
-/**
- * Removes spaces at the begining and end of the
- * sring.
- */
-string strip(const string &str)
-{
-    string res = "";
-    long s = 0;
-    while (s < str.length() && str[s] == ' ')
-    {
-        s++;
-    }
-    long e = str.length() - 1;
-    while (e > s && str[e] == ' ')
-    {
-        e--;
-    }
-    return str.substr(s, e - s + 1);
-}
-
-/**
- * Escapes backslash in the signature of graphs
- */
-string cleanSig(const string &line)
-{
-    int i = 0;
-    string result = "";
-    while (i < line.length() && line[i] != ',')
-    {
-        if (line[i] == '\\')
-        {
-            result += "\\\\";
-        }
-        else
-        {
-            result += line[i];
-        }
-        i++;
-    }
-    result += line.substr(i);
-    return result;
-}
-
-/**
- * Gives the name of the headers given in parameters.
- * Removes the spaces at start and end of each title.
- */
-vector<string> parseHeader(const string &line)
-{
-    if (line.length() == 0)
-    {
-        throw invalid_argument("headers are empty");
-    }
-    vector<string> res;
-    boost::tokenizer<boost::escaped_list_separator<char> > tok(line);
-    boost::tokenizer<boost::escaped_list_separator<char> >::iterator token = tok.begin();
-    for (; token != tok.end(); token++)
-    {
-        res.push_back(strip(*token));
-    }
-    return res;
-}
-
-/**
- * Parse a line of a csv file.
- * Each element is converted in one of the supported format.
- * The results are stored in a map with the name of the headers as keys.
- * The line will be stored with the first value as key (id)
- */
-string parseLine(const string &toParse, const vector<string> &keys, node_list &nodes)
-{
-    if (toParse.length() == 0)
-    {
-        throw invalid_argument("line is empty");
-    }
-    string line = cleanSig(toParse);
-    vector<string> res;
-    boost::tokenizer<boost::escaped_list_separator<char> > tok(line);
-    boost::tokenizer<boost::escaped_list_separator<char> >::iterator token = tok.begin();
-    string sig = *token;
-    ++token;
-    if (!checkFormat(sig))
-    {
-        throw invalid_argument(sig + " is not a valid graph6");
-    }
-    int i = 1;
-    map<string, struct invariant_value> node;
-    unsigned long long int gtint = g6toInt(sig);
-    struct invariant_value val
-    {
-        types::BIGINT, gtint
-    };
-    node.emplace(keys[0], val);
-    for (; token != tok.end(); token++)
-    {
-        if (i > keys.size())
-        {
-            throw invalid_argument("Too many columns");
-        }
-        string p = strip(*token);
-        if (p.size() > 0 && p != "NaN" && p != "inf")
-        {
-            node.emplace(keys[i], parseValue(p));
-        }
-        ++i;
-    }
-    if (i < keys.size())
-    {
-        throw invalid_argument("Not enough columns");
-    }
-    nodes.emplace(sig, node);
-    return sig;
-}
-
-/**
  * Loads the graphs and their properties given in the standard entry in csv format.
  * The headers are the names of each properties and the first property is the id of the graph (graph6)
  * We suppose that the files are in csv format and the first elem is the signature.
@@ -264,22 +62,18 @@ void loadGraphs(vector<string> &keys, node_list &nodes, map<string, jlong> &obje
     int step = 100;
     getline(cin, line);
     keys = parseHeader(line);
-    while (!cin.eof())
-    {
-        try
-        {
+    while (!cin.eof()) {
+        try {
             getline(cin, line);
             string sig = parseLine(line, keys, nodes);
             jlong n = neo->createNode(0, nodes[sig], true);
             //objects.emplace(sig, n);
             graphs.emplace(g6toInt(sig), n);
         }
-        catch (invalid_argument const& e)
-        {
+        catch (invalid_argument const& e) {
             cerr << n << " : " << e.what() << endl;
         }
-        if (n % step == 0)
-        {
+        if (n % step == 0) {
             fprintf(stderr, "%d graphs loaded\n", n);
         }
         n++;
@@ -289,8 +83,7 @@ void loadGraphs(vector<string> &keys, node_list &nodes, map<string, jlong> &obje
 void addTransfos(vector<struct transfo_data_result> &transfos, map<string, jlong> &objects, int threadNum)
 {
     mtx.lock();
-    for (auto trs : transfos)
-    {
+    for (auto trs : transfos) {
         //We suppose that the last filter is trashNode wich always accept
         //And the first one is isGraphInList
         long i = 0;
@@ -301,29 +94,24 @@ void addTransfos(vector<struct transfo_data_result> &transfos, map<string, jlong
         int order[n];
         res = cannonFormOrder(res, order);
         struct filter_result fres = filters[i](res);
-        while (i < filters.size() && !fres.result)
-        {
+        while (i < filters.size() && !fres.result) {
             i++;
             fres = filters[i](res);
         }
         jlong n1 = graphs[trs.start];
         jlong n2;
-        if (!fres.isGraph && !objects.count(fres.dest))
-        {
+        if (!fres.isGraph && !objects.count(fres.dest)) {
             struct invariant_value val = {.type = STRING, .val = fres.dest};
             std::map<string, struct invariant_value> mp;
             mp.emplace("sig", val);
             n2 = neo->createNode(threadNum, mp, false);
             objects.emplace(fres.dest, n2);
         }
-        else
-        {
-            if (!fres.isGraph)
-            {
+        else {
+            if (!fres.isGraph) {
                 n2 = objects[fres.dest];
             }
-            else
-            {
+            else {
                 n2 = graphs[fres.graph];
             }
         }
@@ -339,25 +127,20 @@ void computeTransformations(int threadNum, int start, int inc)
     long n = start;
     long step = max(100, graphs.size() / 100);
     //long step = 1;
-    if (threadNum == 0)
-    {
+    if (threadNum == 0) {
         fprintf(stderr, "  0%%\n");
     }
     auto node = graphs.begin();
     vector<struct transfo_data_result> toAdd;
-    if (n < graphs.size())
-    {
+    if (n < graphs.size()) {
         std::advance(node, start);
     }
-    while (n < graphs.size())
-    {
+    while (n < graphs.size()) {
         Graph g(getOrderBin(node->first));
         binToGraph(node->first, g);
-        for (auto transfo : transfos)
-        {
+        for (auto transfo : transfos) {
             auto results = transfo.transfo(g);
-            for (auto result : results)
-            {
+            for (auto result : results) {
                 //We suppose that the last filter is trashNode wich always accept
                 //And the first one is isGraphInList
                 long i = 0;
@@ -366,17 +149,14 @@ void computeTransformations(int threadNum, int start, int inc)
                 toAdd.push_back(trs);
             }
         }
-        if (n % step < inc)
-        {
+        if (n % step < inc) {
             addTransfos(toAdd, objects, threadNum);
-            if (threadNum == 0)
-            {
+            if (threadNum == 0) {
                 fprintf(stderr, "%3d%%\n", n * 100 / graphs.size());
             }
         }
         n += inc;
-        if (n < graphs.size())
-        {
+        if (n < graphs.size()) {
             std::advance(node, inc);
         }
     }
@@ -423,11 +203,9 @@ int main(int argc, char *argv[])
     //neo = make_shared<Neo4jInserter>();
     neo = make_shared<CsvInserter>();
     vector<string> keys;
-    if (argc > 1)
-    {
+    if (argc > 1) {
         auto cinbuf = cin.rdbuf();
-        for (int i = 1; i < argc; i++)
-        {
+        for (int i = 1; i < argc; i++) {
             cerr << "loading file " << argv[i] << endl;
             ifstream f(argv[i]);
             cin.rdbuf(f.rdbuf());
@@ -435,8 +213,7 @@ int main(int argc, char *argv[])
             f.close();
         }
     }
-    else
-    {
+    else {
         loadGraphs(keys, nodes, objects);
     }
     initTransfos();
@@ -448,13 +225,11 @@ int main(int argc, char *argv[])
     filters.push_back(bind(trashNode, "TRASH", placeholders::_1));
     int ncores = std::thread::hardware_concurrency();
     std::vector<std::thread> threads;
-    for (int i = 0; i < ncores - 1; i++)
-    {
+    for (int i = 0; i < ncores - 1; i++) {
         threads.push_back(std::thread(runThread, i + 1, ncores));
     }
     computeTransformations(0, 0, ncores);
-    for (int i = 0; i < ncores - 1; i++)
-    {
+    for (int i = 0; i < ncores - 1; i++) {
         threads[i].join();
     }
     std::cout << "total : " << ntr << "\n";
